5cc732621bb08838ae837af6f32fc4707b717e3b
qsangdev <quangsang02018@gmail.com>
2023-04-14 16:18:59 +0700

    get API profile & validate edit profile

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,18 +1,32 @@
 import { PoweroffOutlined } from "@ant-design/icons";
-import { Button, Image, Space, Typography } from "antd";
+import { Button, Image, Modal, Space, Typography } from "antd";
 import logo from "../../assets/logo.png";
+import { ExclamationCircleFilled } from "@ant-design/icons/lib/icons";
 
 function AppHeader() {
+  const { confirm } = Modal;
   const handleSignOut = () => {
-    localStorage.removeItem("access_token");
-    localStorage.removeItem("_id");
-    window.location.reload();
+    confirm({
+      title: "Are you sure?",
+      icon: <ExclamationCircleFilled />,
+      content: "Unsaved changes cannot be undone!",
+      onOk() {
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("_id");
+        window.location.reload();
+      },
+      onCancel() {
+        console.log("Cancel");
+      },
+    });
   };
 
   return (
     <div className="AppHeader">
-      <Image width={100} src={logo}></Image>
-      <Typography.Title>Table Now</Typography.Title>
+      <Image style={{ borderRadius: 10 }} width={90} src={logo}></Image>
+      <Typography.Title style={{ marginTop: 10, fontWeight: "600" }}>
+        Table Now
+      </Typography.Title>
       <Space>
         <Button danger onClick={handleSignOut}>
           <PoweroffOutlined />
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,17 @@
 import {
+  CheckCircleOutlined,
   CloseOutlined,
+  EditOutlined,
   ExclamationCircleFilled,
+  UploadOutlined,
 } from "@ant-design/icons/lib/icons";
 import {
-  Alert,
   Button,
   Card,
+  Divider,
   Image,
   Input,
+  InputNumber,
   Layout,
   List,
   Modal,
@@ -22,7 +26,7 @@ import {
 import axios from "axios";
 import moment from "moment";
 import React, { useEffect, useState } from "react";
-import { getImages, getReservation } from "../../API";
+import { getReservation } from "../../API";
 
 const Profile = () => {
   const [messageApi, contextHolder] = message.useMessage();
@@ -38,13 +42,12 @@ const Profile = () => {
   const [disabledDes, setDisabledDes] = useState(true);
   const [loading, setLoading] = useState(false);
   const [dataSource, setDataSource] = useState([]);
-  const [times, setTimes] = useState([]);
-  const [images, setImages] = useState([]);
+  const [dataTimes, setDataTimes] = useState([]);
 
-  const [editImage, setEditImage] = useState(false);
-  const handleEditImage = () => {
-    setEditImage(!editImage);
-  };
+  const [disabledTables, setDisabledTables] = useState(true);
+  const [tables, setTables] = useState("");
+  const [allTables, setAllTables] = useState([]);
+
   const { confirm } = Modal;
 
   const handleEditName = () => {
@@ -63,18 +66,34 @@ const Profile = () => {
     setDisabledDes(false);
   };
 
+  const handleEditTables = () => {
+    setDisabledTables(false);
+  };
+
   useEffect(() => {
     setResID(localStorage.getItem("_id"));
   }, []);
 
-  const showConfirm = async (id) => {
+  const showConfirm = (e) => {
     confirm({
       title: "Do you want to delete this image?",
       icon: <ExclamationCircleFilled />,
-      onOk() {
-        axios
-          .delete(`http://localhost:3000/images/${id}`)
-          .then(getDataImages());
+      onOk: async () => {
+        await axios
+          .post(`http://localhost:3001/api/profile/delete-image/${resID}`, {
+            images: e,
+          })
+          .then((res) => {
+            if (res.data.status === "OK") {
+              message.success(res.data.message, 2.5);
+              getDataDashboard();
+            } else {
+              message.error(res.data.message, 2.5);
+            }
+          })
+          .catch((res) => {
+            message.error(res.data.message, 2.5);
+          });
       },
       onCancel() {
         console.log("Cancel");
@@ -88,7 +107,10 @@ const Profile = () => {
           .put(`http://localhost:3001/api/profile/update/${resID}`, {
             restaurantName: name,
           })
-          .then(() => {
+          .then((res) => {
+            if (res.data.status === "ERR") {
+              return message.error(res.data.message, 2.5);
+            }
             getDataDashboard();
             setDisabledName(true);
           })
@@ -96,32 +118,57 @@ const Profile = () => {
   };
 
   const handleSaveTimes = async (id) => {
-    if (!time) {
-      return alert("Please do not leave it blank!");
+    if (time === "") {
+      return message.warning("Please do not leave it empty!", 2.5);
     } else if (
-      moment(time.openTime).format("HH:mm") < times[id - 1].timeStart &&
-      id !== 1
+      id === 0 &&
+      moment(time.closeTime).format("HH:mm") > dataTimes[id + 1].timeEnd
     ) {
-      return alert("Start time could not before the end of the shift before!");
+      return message.warning(
+        "The end time of this shift must not be after the start of the next shift!",
+        2.5
+      );
     } else if (
-      moment(time.closeTime).format("HH:mm") > times[id].timeStart &&
-      id !== 3
+      id !== 0 &&
+      moment(time.openTime).format("HH:mm") < dataTimes[id - 1].timeEnd
     ) {
-      return alert(
-        "The end time of this shift must not be after the start of the next shift!"
+      return message.warning(
+        "Start time could not before the end of the shift before!",
+        2.5
+      );
+    } else if (
+      id !== 2 &&
+      moment(time.closeTime).format("HH:mm") > dataTimes[id + 1].timeStart
+    ) {
+      return message.warning(
+        "The end time of this shift must not be after the start of the next shift!",
+        2.5
+      );
+    } else if (
+      id === 2 &&
+      moment(time.startTime).format("HH:mm") < dataTimes[id - 1].timeEnd
+    ) {
+      return message.warning(
+        "Start time could not before the end of the shift before!",
+        2.5
       );
     } else {
-      time
+      return time
         ? await axios
-            .patch(`http://localhost:3000/shift/${id}`, {
-              timeStart: moment(time.openTime).format("HH:mm"),
-              timeEnd: moment(time.closeTime).format("HH:mm"),
+            .post(`http://localhost:3001/api/profile/update-time/${resID}`, {
+              shiftTime: [
+                {
+                  shift: id + 1,
+                  timeStart: moment(time.openTime).format("HH:mm"),
+                  timeEnd: moment(time.closeTime).format("HH:mm"),
+                },
+              ],
             })
             .then(() => {
-              getDataTimes();
+              getDataDashboard();
               setDisabledTimesId(disabledTimesId === id);
             })
-        : alert("The new time has not been changed !");
+        : message.error("The new time has not been changed !", 2.5);
     }
   };
 
@@ -145,6 +192,63 @@ const Profile = () => {
       : setDisabledDes(true);
   };
 
+  useEffect(() => {
+    let exportTables = [];
+    for (let i = 0; i < tables; i++) {
+      exportTables[i] = [
+        {
+          shift: 1,
+          name: "Table " + (i + 1),
+          status: "available",
+        },
+        {
+          shift: 2,
+          name: "Table " + (i + 1),
+          status: "available",
+        },
+        {
+          shift: 3,
+          name: "Table " + (i + 1),
+          status: "available",
+        },
+      ];
+    }
+    if (exportTables !== []) {
+      setAllTables(exportTables.flat());
+    }
+  }, [tables]);
+
+  const handleSaveTables = async () => {
+    messageApi.open({
+      type: "loading",
+      content: "Updating..",
+    });
+    tables && allTables !== []
+      ? await axios
+          .put(
+            `http://localhost:3001/api/table/update/${dataSource.restaurantID}`,
+            {
+              tables: allTables,
+            }
+          )
+          .then(async (res) => {
+            if (res.data.status === "ERR") {
+              return message.error(res.data.message, 2.5);
+            } else {
+              await axios.put(
+                `http://localhost:3001/api/profile/update/${resID}`,
+                {
+                  restaurantTable: tables,
+                }
+              );
+              message.success("Updating finished", 2.5);
+              getDataDashboard();
+              setDisabledTables(true);
+            }
+          })
+      : message.error("Missing number of tables", 2.5);
+  };
+
   const getDataDashboard = async () => {
     setLoading(true);
     resID
@@ -152,43 +256,43 @@ const Profile = () => {
           .get(`http://localhost:3001/api/profile/get-details/${resID}`)
           .then((res) => {
             if (res.data.status === "ERR") {
-              return console.log(resID);
+              return message.error(res.data.message, 2.5);
             } else {
               setLoading(false);
               setDataSource(res.data.data);
+              setDataTimes(res.data.data.shiftTime);
             }
           })
           .catch((err) => console.log(err))
-      : console.log("loading..");
-  };
-
-  const getDataImages = () => {
-    setLoading(true);
-    getImages().then((res) => {
-      setImages(res.data);
-      setLoading(false);
-    });
-  };
-
-  const getDataTimes = () => {
-    setLoading(true);
-    getReservation().then((res) => {
-      setTimes(res.data);
-      setLoading(false);
-    });
+      : messageApi.open({
+          type: "loading",
+          content: "Loading..",
+        });
   };
 
   useEffect(() => {
     getDataDashboard();
   }, [resID]);
 
-  useEffect(() => {
-    getDataImages();
-  }, []);
+  const handleUpload = (e) => {
+    if (!(e.event instanceof ProgressEvent)) return;
 
-  useEffect(() => {
-    getDataTimes();
-  }, []);
+    let bodyFormData = new FormData();
+    bodyFormData.append("images", e.file.originFileObj);
+    messageApi.open({
+      type: "loading",
+      content: "Uploading image..",
+      duration: 2.5,
+    });
+    axios
+      .post(`http://localhost:3001/api/profile/upload/${resID}`, bodyFormData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+      .then((res) => {
+        message.success("Uploading finished", 2.5);
+        getDataDashboard();
+      });
+  };
 
   return (
     <>
@@ -200,7 +304,7 @@ const Profile = () => {
             title={"Name"}
             size="small"
             loading={loading}
-            style={{ marginTop: 10, width: "700px" }}
+            style={{ marginTop: 10, width: "750px" }}
           >
             <Input
               style={{ width: "600px", marginRight: 10, height: "39px" }}
@@ -227,70 +331,7 @@ const Profile = () => {
               </Button>
             )}
           </Card>
-          <Card
-            title="Time Shifts"
-            size="small"
-            loading={loading}
-            style={{ marginTop: 10 }}
-          >
-            {times.length > 0 ? (
-              times.map((e) => {
-                return (
-                  <Layout
-                    key={e.id}
-                    style={{
-                      flexDirection: "row",
-                      padding: 5,
-                    }}
-                  >
-                    <Typography.Title
-                      level={5}
-                      style={{ marginTop: 9, marginLeft: 30, marginRight: 30 }}
-                    >
-                      Shift {e.id}: {times[e.id - 1].timeStart} -{" "}
-                      {times[e.id - 1].timeEnd}
-                    </Typography.Title>
-                    <TimePicker.RangePicker
-                      format={"HH:mm"}
-                      disabled={disabledTimesId !== e.id}
-                      style={{
-                        width: "390px",
-                        marginRight: 10,
-                        height: "39px",
-                      }}
-                      status="error"
-                      onChange={(e) =>
-                        setTime({
-                          openTime: e[0].$d,
-                          closeTime: e[1].$d,
-                        })
-                      }
-                    />
-                    {disabledTimesId === e.id ? (
-                      <Button
-                        style={{ height: "40px" }}
-                        type="primary"
-                        danger
-                        onClick={() => handleSaveTimes(e.id)}
-                      >
-                        Save
-                      </Button>
-                    ) : (
-                      <Button
-                        style={{ height: "40px" }}
-                        type="primary"
-                        onClick={() => handleEditTimes(e.id)}
-                      >
-                        Edit
-                      </Button>
-                    )}
-                  </Layout>
-                );
-              })
-            ) : (
-              <Spin></Spin>
-            )}
-          </Card>
+
           <Card
             title="Address"
             size="small"
@@ -353,90 +394,170 @@ const Profile = () => {
               </Button>
             )}
           </Card>
+          <Card
+            title="Number of Tables"
+            size="small"
+            loading={loading}
+            style={{ marginTop: 10 }}
+          >
+            <Space>
+              <Typography.Title level={5} style={{ marginRight: "378px" }}>
+                Current : {dataSource.restaurantTable} Tables
+              </Typography.Title>
+              <InputNumber
+                min={1}
+                max={30}
+                size="large"
+                defaultValue={dataSource.restaurantTable}
+                disabled={disabledTables}
+                onChange={(e) => setTables(e)}
+                style={{
+                  fontSize: 25,
+                  marginRight: 2,
+                }}
+              />
+
+              {disabledTables === false ? (
+                <Button
+                  style={{ height: "40px" }}
+                  type="primary"
+                  danger
+                  onClick={handleSaveTables}
+                >
+                  Save
+                </Button>
+              ) : (
+                <Button
+                  style={{ height: "40px" }}
+                  type="primary"
+                  onClick={handleEditTables}
+                >
+                  Edit
+                </Button>
+              )}
+            </Space>
+          </Card>
+          <Card
+            title="Shift Time"
+            size="small"
+            loading={loading}
+            style={{ marginTop: 10 }}
+          >
+            {dataTimes ? (
+              dataTimes.map((e) => {
+                return (
+                  <Divider
+                    key={e.shift}
+                    style={{
+                      flexDirection: "row",
+                    }}
+                  >
+                    <Typography.Title
+                      level={5}
+                      style={{ marginTop: 9, marginLeft: 30, marginRight: 30 }}
+                    >
+                      Shift {e.shift} : {dataTimes[e.shift - 1].timeStart} -{" "}
+                      {dataTimes[e.shift - 1].timeEnd}
+                    </Typography.Title>
+                    <TimePicker.RangePicker
+                      allowClear={false}
+                      format={"HH:mm"}
+                      disabled={disabledTimesId !== e.shift}
+                      style={{
+                        width: "390px",
+                        marginRight: 10,
+                        height: "39px",
+                      }}
+                      status="error"
+                      onChange={(e) =>
+                        setTime({
+                          openTime: e[0].$d,
+                          closeTime: e[1].$d,
+                        })
+                      }
+                    />
+                    {disabledTimesId === e.shift ? (
+                      <Button
+                        style={{ height: "40px" }}
+                        type="primary"
+                        danger
+                        onClick={() => handleSaveTimes(dataTimes.indexOf(e))}
+                      >
+                        Save
+                      </Button>
+                    ) : (
+                      <Button
+                        style={{ height: "40px" }}
+                        type="primary"
+                        onClick={() => handleEditTimes(e.shift)}
+                      >
+                        Edit
+                      </Button>
+                    )}
+                  </Divider>
+                );
+              })
+            ) : (
+              <Spin></Spin>
+            )}
+          </Card>
         </Layout>
 
         <Typography.Title level={3}>Images</Typography.Title>
         <Layout style={{ flexDirection: "row" }}>
-          {editImage ? (
-            <Button onClick={handleEditImage} style={{ margin: 10 }} danger>
-              Done
+          <Upload
+            key="upload-image"
+            showUploadList={false}
+            multiple={true}
+            onChange={handleUpload}
+          >
+            <Button
+              icon={<UploadOutlined />}
+              style={{ margin: 10, width: 100 }}
+            >
+              Upload
             </Button>
-          ) : (
-            <Button onClick={handleEditImage} style={{ margin: 10 }}>
-              Edit
-            </Button>
-          )}
-          <Upload>
-            <Button style={{ margin: 10 }}>Upload</Button>
           </Upload>
         </Layout>
-        {editImage ? (
-          <List
-            style={{}}
-            loading={loading}
-            dataSource={images}
-            grid={{
-              xs: 1,
-              sm: 1.5,
-              md: 2,
-              lg: 3,
-              xl: 3.5,
-              xxl: 4,
-            }}
-            renderItem={(e) => {
-              return (
-                <Card
-                  key={e.id}
-                  style={{
-                    margin: 10,
-                  }}
-                >
-                  <Tooltip title="delete">
-                    <Button
-                      onClick={() => showConfirm(e.id)}
-                      style={{
-                        position: "absolute",
-                        right: 0,
-                        top: 0,
-                      }}
-                      type="primary"
-                      shape="circle"
-                      danger
-                      icon={<CloseOutlined />}
-                    />
-                  </Tooltip>
-                  <Image src={e.src}></Image>
-                </Card>
-              );
-            }}
-          ></List>
-        ) : (
-          <List
-            style={{}}
-            loading={loading}
-            dataSource={images}
-            grid={{
-              xs: 1,
-              sm: 1.5,
-              md: 2,
-              lg: 3,
-              xl: 3.5,
-              xxl: 4,
-            }}
-            renderItem={(e) => {
-              return (
-                <Card
-                  key={e.id}
-                  style={{
-                    margin: 10,
-                  }}
-                >
-                  <Image src={e.src}></Image>
-                </Card>
-              );
-            }}
-          ></List>
-        )}
+
+        <List
+          loading={loading}
+          dataSource={dataSource.images}
+          grid={{
+            xs: 1,
+            sm: 1.5,
+            md: 2,
+            lg: 3,
+            xl: 3.5,
+            xxl: 4,
+          }}
+          renderItem={(e) => {
+            return (
+              <Card
+                key={e.id}
+                style={{
+                  margin: 10,
+                }}
+              >
+                <Tooltip title="delete">
+                  <Button
+                    onClick={() => showConfirm(e)}
+                    style={{
+                      position: "absolute",
+                      right: 0,
+                      top: 0,
+                    }}
+                    type="primary"
+                    shape="circle"
+                    danger
+                    icon={<CloseOutlined />}
+                  />
+                </Tooltip>
+                <Image src={e}></Image>
+              </Card>
+            );
+          }}
+        ></List>
       </Space>
     </>
   );
